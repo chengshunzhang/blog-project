02292b08cb47b38b84f4da2561b3aba541be1f14
chengshunzhang <chengshun.zhang@outlook.com>
2019-01-05 23:04:28 -0800

    AboutView and PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView, ListView
+from blog.models import Post, Comment
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
